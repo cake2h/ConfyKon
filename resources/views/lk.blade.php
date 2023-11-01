3b5984d220bb1b72e570b5f03bc585d3a3ee901e
qidemoto <dima.glushenkov04@gmail.com>
2023-11-02 01:35:38 +0500

    Вынос стилей 1

--- resources/views/lk.blade.php
+++ resources/views/lk.blade.php
@@ -16,20 +16,20 @@
 <body>
 <header class="header-buttons">
   <nav>
-    <a href="https://www.utmn.ru/" style="margin-left: 14px; margin-right: 14px;">
-      <svg class="h-[45px]" style="fill: #3D423D" viewBox="0 0 185.9 83.8">
+    <a href="https://www.utmn.ru/">
+      <svg class="h-[45px]" viewBox="0 0 185.9 83.8">
         <path class="st0" d="M78.3,41.8h-8v22.6h-5.7V41.8h-8v-5.1h21.7V41.8z M139.2,50.2l1,14.1h5.2l-1.8-23.9h-0.2h-5l-7.9,18.1   l-7.7-18.1h-5h-0.2l-1.8,23.9h5.2l1-13.9l5.9,13.9h5L139.2,50.2z M100.8,40.2c-6,0-11,4.3-12.1,9.9h-2.6v-9.7h-5.7v23.9h5.7v-9.1   h2.7c1.2,5.5,6.1,9.6,12,9.6c6.8,0,12.3-5.5,12.3-12.3S107.6,40.2,100.8,40.2 M100.8,45.4c3.9,0,7.1,3.2,7.1,7.1s-3.2,7.1-7.1,7.1   s-7.1-3.2-7.1-7.1S96.9,45.4,100.8,45.4 M165.6,36.7l7.7,18.5l-0.7,1.9c-0.6,1.5-1,3.3-4.4,3.3v5.1c5.9,0,7.6-3.1,9.7-8.4l8-20.4   h-5.8L176,49.4l-4.5-12.7H165.6z M155.1,41.8v22.6h-5.7V36.8l0,0h15.2v5.1L155.1,41.8L155.1,41.8z"></path>
         <polygon class="st0" points="52.8,7.6 39.6,0 0,22.8 0,38.1 13.2,45.7 13.2,30.5  "></polygon>
         <polygon class="st0" points="26.4,83.8 26.4,38.1 13.2,45.7 13.2,76.2  "></polygon>
       </svg>
     </a>
     <div>
-        <a href="{{ route('conf.index') }}" style="margin-left: 14px; margin-right: 14px;">Список конференций</a>
+        <a href="{{ route('conf.index') }}">Список конференций</a>
         @if (Auth::user()->isAdmin())
-            <a href="{{ route('admin.index') }}" style="margin-left: 14px; margin-right: 14px;">Админ-панель</a>
+            <a href="{{ route('admin.index') }}">Админ-панель</a>
         @endif
 
-        <a href="#" style="margin-left: 14px; margin-right: 14px;" onclick="event.preventDefault(); document.getElementById('logoutForm').submit();">Выход</a>
+        <a href="#" onclick="event.preventDefault(); document.getElementById('logoutForm').submit();">Выход</a>
         <form method="POST" action="{{ route('logout') }}" id="logoutForm" style="display: none;">
             @csrf
         </form>
@@ -39,7 +39,7 @@
 
 <div class="container">
     <div class="user-info-container">
-        <p><h3><strong>{{$user->surname}} {{$user->name}} {{$user->midname}}</strong></h3></p>
+        <strong><h2>{{$user->surname}} {{$user->name}} {{$user->midname}}</h></strong>
         <p><strong>Дата рождения:</strong> {{$user->birthday}}</p>
         <p><strong>Почта:</strong> {{$user->email}}</p>
         <p><strong>Город:</strong> {{$user->city}}</p>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,8 +16,8 @@
 <body>
 <header class="header-buttons">
   <nav>
-    <a href="https://www.utmn.ru/" style="margin-left: 14px; margin-right: 14px;">
-      <svg class="h-[45px]" style="fill: #3D423D" viewBox="0 0 185.9 83.8">
+    <a href="https://www.utmn.ru/">
+      <svg class="h-[45px]" viewBox="0 0 185.9 83.8">
         <path class="st0" d="M78.3,41.8h-8v22.6h-5.7V41.8h-8v-5.1h21.7V41.8z M139.2,50.2l1,14.1h5.2l-1.8-23.9h-0.2h-5l-7.9,18.1   l-7.7-18.1h-5h-0.2l-1.8,23.9h5.2l1-13.9l5.9,13.9h5L139.2,50.2z M100.8,40.2c-6,0-11,4.3-12.1,9.9h-2.6v-9.7h-5.7v23.9h5.7v-9.1   h2.7c1.2,5.5,6.1,9.6,12,9.6c6.8,0,12.3-5.5,12.3-12.3S107.6,40.2,100.8,40.2 M100.8,45.4c3.9,0,7.1,3.2,7.1,7.1s-3.2,7.1-7.1,7.1   s-7.1-3.2-7.1-7.1S96.9,45.4,100.8,45.4 M165.6,36.7l7.7,18.5l-0.7,1.9c-0.6,1.5-1,3.3-4.4,3.3v5.1c5.9,0,7.6-3.1,9.7-8.4l8-20.4   h-5.8L176,49.4l-4.5-12.7H165.6z M155.1,41.8v22.6h-5.7V36.8l0,0h15.2v5.1L155.1,41.8L155.1,41.8z"></path>
         <polygon class="st0" points="52.8,7.6 39.6,0 0,22.8 0,38.1 13.2,45.7 13.2,30.5  "></polygon>
         <polygon class="st0" points="26.4,83.8 26.4,38.1 13.2,45.7 13.2,76.2  "></polygon>
@@ -25,15 +25,15 @@
     </a>
     <div >
         @if(Auth::check())
-            <a href="{{ route('lk') }}" style="margin-left: 14px; margin-right: 14px;">Личный кабинет</a>
-            <a href="#" style="margin-left: 14px; margin-right: 14px;" 
+            <a href="{{ route('lk') }}">Личный кабинет</a>
+            <a href="#" 
                 onclick="event.preventDefault(); document.getElementById('logoutForm').submit();">Выход</a>
             <form method="POST" action="{{ route('logout') }}" id="logoutForm" style="display: none;">
                 @csrf
             </form>
         @else
-            <a href="{{ route('login') }}" style="margin-left: 14px; margin-right: 14px;">Вход</a>
-            <a href="{{ route('register') }}" style="margin-left: 14px; margin-right: 14px;">Регистрация</a>
+            <a href="{{ route('login') }}">Вход</a>
+            <a href="{{ route('register') }}">Регистрация</a>
         @endif
     </div>
   </nav>
@@ -42,7 +42,7 @@
 <div class="centrator">
   <div class="container">
   @foreach ($confs as $conf)
-    <section id='{{ $conf->id }}' style="background-color: rgb(4, 159, 217, 0.5)" class="program-section section-hoverable font-size">
+    <section id='{{ $conf->id }}' class="program-section section-hoverable font-size">
       <h2 style>{{ $conf->name }}</h2>
       <div class="info-box">
         <p>Тут будет дополнительная информация о секции.</p>
